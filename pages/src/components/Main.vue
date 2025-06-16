7295effc9c34bc973a362fd1cbdc6b7330ac12de
bolixinyu <2410781627@qq.com>
2025-06-16 17:28:20 +0800

    project page

---
+++ pages/src/components/Main.vue
@@ -0,0 +1,27 @@
+<script>
+import Title from './sections/Title.vue'
+import Abstract from './sections/Abstract.vue'
+import Overview from './sections/Overview.vue'
+import Detail from './sections/Detail.vue'
+import Result from './sections/Result.vue'
+import BibTeX from './sections/BibTeX.vue'
+export default {
+  components: {
+    Title,
+    Abstract,
+    Overview,
+	Detail,
+	Result,
+    BibTeX
+  }
+}
+</script>
+
+<template>
+  <Title/>
+  <Abstract/>
+  <Overview/>
+  <Detail/>
+  <Result/>
+  <BibTeX/>
+</template>
---
+++ pages/src/components/sections/BibTeX.vue
@@ -0,0 +1,91 @@
+<script>
+export default {
+  data() {
+    return {
+      bibtex: [
+        "@article{you2024towards,",
+        "  title={Towards Long Video Understanding via Fine-detailed Video Story Generation},",
+        "  author={You, Zeng and Wen, Zhiquan and Chen, Yaofo and Li, Xin and Zeng, Runhao and Wang, Yaowei and Tan, Mingkui},",
+        "  journal={IEEE Transactions on Circuits and Systems for Video Technology},",
+        "  year={2024}",
+        "}",
+      ],
+    }
+  },
+  methods: {
+    copyVal() {
+      let oInput = document.createElement('textarea');
+      let text = document.getElementById('bibtex').innerText;
+      oInput.value = text;
+      document.body.appendChild(oInput);
+      oInput.select();
+      document.execCommand('Copy');
+      this.$message.success('Copy Successfully');
+      oInput.remove();
+    }
+  }
+}
+
+</script>
+
+<template>
+  <div>
+    <el-divider />
+      
+      <el-row justify="center">
+        <h1 class="section-title">BibTeX</h1>
+      </el-row>
+      
+      <el-row justify="center">
+        <el-col class='bibtex' :xs="24" :sm="20" :md="14" :lg="12" :xl="12" @click="copyVal()" >
+          <div style="text-align: center; color: var(--el-text-color-secondary); margin-top: 20px;">🖱️ Click here to copy BibTex.</div> 
+          <el-row>
+              <el-scrollbar style="margin: 0px 20px 5px 20px;">
+                <pre id="bibtex"><code v-for="b in bibtex">{{ b }}<br/></code></pre>
+              </el-scrollbar>
+          </el-row>
+        </el-col>
+      </el-row>
+
+  </div>
+</template>
+
+<style scoped>
+
+.scrollbar-flex-content {
+  display: flex;
+}
+
+/* 卡片属性 */
+.bibtex {
+  margin: 20px 0px;
+  padding-top: 5px;
+  box-shadow: var(--el-box-shadow-light); 
+  border-radius: 10px;
+}
+
+/* 卡片悬浮 */
+.bibtex:hover {
+  box-shadow: var(--el-box-shadow); 
+}
+
+/* 卡片点击 */
+.bibtex:active{
+  box-shadow: var(--el-box-shadow-lighter); 
+}
+
+pre {
+  border: none;
+  border-radius: 0px;
+  padding: 10px;
+  background: none;
+}
+
+pre code {
+  font-size: 18px;
+  background: #ffffff;
+}
+
+
+
+</style>
